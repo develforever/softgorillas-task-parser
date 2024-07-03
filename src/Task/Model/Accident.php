6eec05dc8e0c0b1f5182164c0ec8d6b065cf8b2b
Develforever <rockfade@gmail.com>
2024-07-03 21:39:07 +0200

    add enums

--- src/Task/Model/Accident.php
+++ src/Task/Model/Accident.php
@@ -4,17 +4,6 @@ namespace App\Task\Model;
 
 class Accident extends AbstractModel implements ModelInterface
 {
-    public const PRIORITY_NORMAL = 'normal';
-    public const PRIORITY_HIGH = 'high';
-    public const PRIORITY_URGENT = 'urgent';
-
-    public const STATUS_NEW = 'new';
-    public const STATUS_DATE_OF_INSPECTION = 'date_of_inspection';
-    public const STATUS_IN_PROGRESS = 'in_progress';
-    public const STATUS_DONE_OK = 'done_ok';
-    public const STATUS_DONE_FAILED = 'done_failed';
-
-    public const TYPE = 'accident';
 
     public $serviceVisitDate;
 }
--- src/Task/Model/Inspection.php
+++ src/Task/Model/Inspection.php
@@ -4,18 +4,7 @@ namespace App\Task\Model;
 
 class Inspection extends AbstractModel implements ModelInterface
 {
-    public const PRIORITY_NORMAL = 'normal';
-    public const PRIORITY_HIGH = 'high';
-    public const PRIORITY_URGENT = 'urgent';
-
-    public const STATUS_NEW = 'new';
-    public const STATUS_AUTO_SHEDULED = 'auto_sheduled';
-    public const STATUS_IN_PROGRESS = 'in_progress';
-    public const STATUS_DONE_OK = 'done_ok';
-    public const STATUS_DONE_FAILED = 'done_failed';
-
-    public const TYPE = 'inspection';
-
+  
     public $inspectionDate;
     public $weekNumber;
 }
